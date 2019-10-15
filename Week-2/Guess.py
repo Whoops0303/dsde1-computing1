465c9588d98e0be32209ada68cda2e04b0b68e54
Tristan Lim ;) <twl319@ic.ac.uk>
2019-10-15 12:03:08 +0100

    First Commit, Upload Week2

---
+++ Week-2/Guess.py
@@ -0,0 +1,5 @@
+print("1 to 10, gimme number!")
+guess = int(input())
+import random
+ans = random.randint(1,11)
+print(guess==ans)
---
+++ Week-2/firstProgram.py
@@ -0,0 +1,4 @@
+message = 'What is your message?'
+print(message)
+user_spam = input()
+print(user_spam)
