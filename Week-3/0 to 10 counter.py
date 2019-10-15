f45bf72038bf13eddc0f2d2f44b4ee4a4965a52a
Tristan Lim ;) <twl319@ic.ac.uk>
2019-10-15 15:41:40 +0100

    Added 0 to 10 counter and flowchart

---
+++ Week-3/0 to 10 counter.py
@@ -0,0 +1,19 @@
+# 0 --> 10 using while loop
+counter = 0
+while counter <= 10:
+    print(counter)
+    counter += 1
+
+# 10 --> 0 using while loop
+counter = 10
+while counter >= 0:
+    print(counter)
+    counter += -1
+
+# 0 --> 10 using for loop
+for num in range(0,11):
+    print(num)
+
+# 10 --> 0 using for loop
+for num in range (10,-1,-1):
+    print(num)
