4b38820e150e8209a15260c13fab160543870768
Tristan Lim ;) <twl319@ic.ac.uk>
2019-10-17 16:35:09 +0100

    Pendulum and toys Exercise

---
+++ Week-3/pendulum.py
@@ -0,0 +1,15 @@
+import math
+
+def period(L,g):
+    try:
+        T = math.pi*2*(L/g)**0.5
+        return T
+    except ZeroDivisionError:
+        print("You can't divide by zero bruh")
+        return "Undefined"
+    except TypeError:
+        print("Please enter a number")
+        return "Like 1,2,3"
+    except NameError:
+        print("Please enter a number")
+        return "Like 1,2,3"
