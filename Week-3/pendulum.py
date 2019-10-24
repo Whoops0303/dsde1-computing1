516ae917b33eb932ccfbc60787e074bb5432dd0a
Tristan Lim ;) <twl319@ic.ac.uk>
2019-10-24 16:20:57 +0100

    Completed most things

--- Week-3/pendulum.py
+++ Week-3/pendulum.py
@@ -2,10 +2,11 @@ import math
 
 def period(L,g):
     try:
-        T = math.pi*2*(L/g)**0.5
+        if g == 0:
+            raise ValueError("You can't divide by zero bruh")
+        else:
+            T = math.pi*2*(L/g)**0.5
         return T
-    except ZeroDivisionError:
-        raise ZeroDivisionError("You can't divide by zero bruh")
     except TypeError:
         raise TypeError("Please enter a number")
     except NameError:
--- Week-4/keywords.py
+++ Week-4/keywords.py
@@ -15,38 +15,41 @@ with default values.
 # if both user_name and place are provided
 # it returns 'Hello, <user_name>, and welcome to <place>
 
-def welcome_message(user_name == None ,place == None):
-    if user_name != None and place != None:
+def welcome_message(user_name=None, place=None):
+    '''This function outputs messages and accepts username and place input'''
+    if user_name is not None and place is not None:
         return "Hello, " + user_name + ", and welcome to " + place
-    elif user_name != None:
+    if user_name is not None:
         return "Hello, " + user_name + ", and welcome"
-    elif place != None:
+    if place is not None:
         return "Hello and welcome to " + place
-    else:
-        return "Hello and welcome"
-
+    return "Hello and welcome"
 
 # Create a function called list_average()
-# without using any libraries to do the maths for you 
-# (the point of this exercise is to practice interacting 
+# without using any libraries to do the maths for you
+# (the point of this exercise is to practice interacting
 # with lists)
 # the first argument is a list of numbers
 # the second optional keyword arguemt is called avg_type
 # if nothing for avg_type provided, return the mean of the list
-# if avg_type='mode', return the mode of the list 
+# if avg_type='mode', return the mode of the list
 # (return list of all modes if there is a tie between multiple values)
 # if avg_type='mean', return the mean of the list
 # if avg_type='median', return the median of this list
-def list_average(lst, avg_type == None):
+def list_average(lst, avg_type=None):
+    '''This annoying long piece of junk gives you the mean, median and mode'''
     mean_total = 0
     types = []
     mode_count = 0
     modes = []
-    if avg_type == None or avg_type == 'mean':
+    sorted_list = sorted(lst)
+    if avg_type in (None, 'mean'):
+        if not lst:
+            return 0
         for number in lst:
             mean_total += number
         return mean_total/len(lst)
-    elif avg_type == 'mode':
+    if avg_type == 'mode':
         for number in lst:
             if number not in types:
                 types.append(number)
@@ -57,15 +60,10 @@ def list_average(lst, avg_type == None):
             if lst.count(number) == mode_count:
                 modes.append(number)
         return modes
-    elif avg_type == median:
-        if len(lst)%2 == 0:
-            return (lst[len(lst)/2-1] + lst[len(lst)/2])/2
-        else:
-            return lst[(len(lst)-1)/2]
-
-            
-
-        
-
-
-        
+    if avg_type == 'median':
+        if not lst:
+            return None
+        if len(sorted_list)%2 == 0:
+            return (sorted_list[int(len(lst)/2-1)] + sorted_list[int(len(lst)/2)])/2
+        return sorted_list[int((len(lst)-1)/2)]
+      
