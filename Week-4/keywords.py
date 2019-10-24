39bff7e17c3d06641da4bfa0d7b07c2c258c3890
Tristan Lim ;) <twl319@ic.ac.uk>
2019-10-24 15:00:01 +0100

    Done keywords

--- Week-4/keywords.py
+++ Week-4/keywords.py
@@ -15,6 +15,16 @@ with default values.
 # if both user_name and place are provided
 # it returns 'Hello, <user_name>, and welcome to <place>
 
+def welcome_message(user_name == None ,place == None):
+    if user_name != None and place != None:
+        return "Hello, " + user_name + ", and welcome to " + place
+    elif user_name != None:
+        return "Hello, " + user_name + ", and welcome"
+    elif place != None:
+        return "Hello and welcome to " + place
+    else:
+        return "Hello and welcome"
+
 
 # Create a function called list_average()
 # without using any libraries to do the maths for you 
@@ -27,3 +37,35 @@ with default values.
 # (return list of all modes if there is a tie between multiple values)
 # if avg_type='mean', return the mean of the list
 # if avg_type='median', return the median of this list
+def list_average(lst, avg_type == None):
+    mean_total = 0
+    types = []
+    mode_count = 0
+    modes = []
+    if avg_type == None or avg_type == 'mean':
+        for number in lst:
+            mean_total += number
+        return mean_total/len(lst)
+    elif avg_type == 'mode':
+        for number in lst:
+            if number not in types:
+                types.append(number)
+        for number in types:
+            if lst.count(number) >= mode_count:
+                mode_count = lst.count(number)
+        for number in types:
+            if lst.count(number) == mode_count:
+                modes.append(number)
+        return modes
+    elif avg_type == median:
+        if len(lst)%2 == 0:
+            return (lst[len(lst)/2-1] + lst[len(lst)/2])/2
+        else:
+            return lst[(len(lst)-1)/2]
+
+            
+
+        
+
+
+        
